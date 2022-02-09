2f31306d7d0c32efb278a38a7009b4e0d8fd7241
hj.ha <hj.ha@saigeresearch.ai>
2022-02-09 19:55:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# documentation
+
+* links
+
+  * [pdoc](https://github.com/pdoc3/pdoc)
+
+  * [blog post - tds](https://towardsdatascience.com/how-to-generate-professional-api-docs-in-minutes-from-docstrings-aed0341bbda7)
+
+# run
+
+```console
+pdoc math-func.py
+# # optional 
+# -h 0.0.0.0 : for host 
+# -p 8080 : for port
+# --math : for latex
+# -o [LOCATION] : for exporting HTML. ex) ``-o docs``
+```
---
+++ math-func.py
@@ -0,0 +1,53 @@
+def addition(num1, num2=0):
+    """
+    Adds two numbers
+    
+    Args:
+    
+    num1: The first number
+    
+    num2: The second number, default 0
+    
+    Returns:
+    
+    The result of the addition process
+    """
+    
+    return (num1+num2)
+
+def mult(num1, num2=1):
+    """
+    Multiplies two numbers
+    
+    Args:
+    
+    `num1`: The first number
+        
+    `num2`: The second number, default `1`
+    
+    Returns:
+    
+    The result of the **multiplication** process
+    """
+    
+    return (num1*num2)
+
+def pythagorus(num1, num2=0):
+    """
+    ### Description:
+    
+    Calculates the root-sum-of-square of two numbers
+    
+    ### Args:
+    
+    `num1`: The first number
+        
+    `num2`: The second number, default `0`
+    
+    ### Returns:
+    
+    The result of the **pythagorus** formula
+    
+    $$dist = \\sqrt { a^2+b^2 }$$
+    """
+    return (num1**2 + num2**2)**0.5
